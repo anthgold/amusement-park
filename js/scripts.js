f01b3fbf32be05524b4ca7394b95b2ef23b50a7d
Anthony Gold and Tommy Jones <anthony@>
2016-03-24 10:15:06 -0700

    changes div ids to div classes, to allow re-use of div tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-    var height = parseInt(prompt("How tall are you in inches?"));
+  var height = parseInt(prompt("How tall are you in inches?"));
 
   if (height < 48) {
-    $('#too-short-rides').show();
+    $('.too-short-rides').show();
   } else if (height > 70) {
-    $('#too-tall-rides').show();
+    $('.too-tall-rides').show();
   } else {
-    $('#all-rides').show();
+    $('.all-rides').show();
   }
-event.preventDefault();
+  event.preventDefault();
+
 });
